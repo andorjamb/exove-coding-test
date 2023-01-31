e72b2557a82fa539cd0c2a340eecb7b357d732f6
Anna Petelin <andorjamb@protonmail.com>
2023-01-31 21:00:15 +0200

    initial commit

---
+++ section1/1A_asciiSort/1a_asciiSort.js
@@ -0,0 +1,11 @@
+/** Write a sorting algorithm that sorts strings based on the ASCII code of the third letter first, then second letter and then the first letter.
+
+Here’s a list of words you can test with:
+
+BOAT
+Locomotive
+Poet
+Accelerate
+GOLF
+ACCIDENTAL
+Submarine */
---
+++ section2/2B_sql/tables.sql
@@ -0,0 +1,4 @@
+CREATE TABLE IF NOT EXISTS people;
+CREATE TABLE IF NOT EXISTS phones;
+use people;
+insert into people 
