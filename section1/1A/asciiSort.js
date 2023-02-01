ee6c6e982ad69a55939ae3baa548fe4213007e16
Anna Petelin <andorjamb@protonmail.com>
2023-02-01 14:34:55 +0200

    finalize react-slider

--- section1/1A/asciiSort.js
+++ section1/1A/asciiSort.js
@@ -1,7 +1,7 @@
 /** Write a sorting algorithm that sorts strings based on the ASCII code of the third letter first, then second letter and then the first letter.
 */
 
-const stringArray = ['BOAT', 'Locomotive', 'Poet', 'Accelerate', 'GOLF', 'ACCIDENTAL', 'Submarine'];
+const stringArray = ['BOAT', 'Locomotive', 'Poet', 'Accelerate', 'GOLF', 'ACCIDENTAL', 'Submarine', 'ghost'];
 main();
 
 function asciiConverter(input) {
@@ -10,7 +10,7 @@ function asciiConverter(input) {
     let asciified = inputArr.map((char) => char.charCodeAt(0));
     return asciified;
 }
-
+/*
 function buildArray2d() {
     //push the arrays of asciified values to a two-dimensional array
     let array2d = [];
@@ -19,13 +19,75 @@ function buildArray2d() {
         console.log(array2d);
     }
     return array2d;
-}
-function compareArrays(arr) {
-    //sorting algorithm
+} * /
 
+// Add to a specified location
+//Array.splice(start_position, 0, new_element...);
+
+
+*/
+
+//divide and conquer
+function mergeSort(arr) {
+    if (arr.length <= 1) { console.log(arr); return arr };
+    arrMidpoint = Math.floor(arr.length / 2);
+    let firstHalf = mergeSort(arr.slice(0, arrMidpoint)); //recursive
+    console.log(firstHalf);
+    let secondHalf = mergeSort(arr.slice(arrMidpoint));
+    console.log(secondHalf);
+    mergeArrays(firstHalf, secondHalf);
+
+    function mergeArrays(arr1, arr2) {
+        let sorted = [];
+        let i = 0;
+        let j = 0;
+        console.log(arr1);
+        console.log(arr2);
+        while (i < arr1.length && j < arr2.length) {
+            if (asciiConverter(arr1[i])[2] < asciiConverter(arr2[j])[2]) {
+                sorted.push(arr1[i]);
+                console.log(sorted);
+                i++;
+            }
+            /*   if (arr2[j] > arr1[i]) {
+                  results.push(arr1[i]);
+                  i++;
+              } */
+            else {
+                sorted.push(arr2[j])
+                j++;
+            }
+        }
+        while (i < arr1.length) {
+            sorted.push(arr1[i])
+            i++;
+        }
+        while (j < arr2.length) {
+            sorted.push(arr2[j])
+            j++;
+        }
+        return sorted;
+
+
+        /*    else if (arr1.charCodeAt(2) === sorted[0].charCodeAt(2)) {
+               if (arr1.charCodeAt(1) < sorted[0].charCodeAt(1)) {
+                   sorted.unshift(arr1)
+                   console.log(sorted);
+               }
+               else if (arr1.charCodeAt(1) === sorted[0].charCodeAt(1)) {
+                   if (arr1.charCodeAt(0) < sorted[0].charCodeAt(0))
+                       sorted.unshift(arr1)
+                   console.log(sorted);
+               } */
+        //}
+
+    }
 }
 
 function main() {
-    compareArrays(buildArray2d());
-
+    mergeSort(stringArray);
 }
+
+
+
+
--- section1/1C/react-slider/src/App.js
+++ section1/1C/react-slider/src/App.js
@@ -1,37 +1,28 @@
-import { useState, useEffect, useRef } from 'react';
+import { useState, useRef } from 'react';
 import './App.css';
-/**
- * 1C. Create a slider in react
-Have its value stored in state
-Have it fire an event with the value
-Have it fire the event only when the user has stopped moving it
- */
-
 
 function App() {
 
   const [slider, setSlider] = useState(0);
+  const [opacity, setOpacity] = useState(1);
   const sliderThumb = useRef();
+  const displayDiv = useRef();
 
   const slideHandler = () => {
-    setSlider(sliderThumb.current.value);
+    setSlider(+sliderThumb.current.value);
   }
 
-  const blurHandler = () => {
-    console.log('stopped');
+  const mouseUpHandler = () => {
+    setOpacity(slider / 100);
   }
-
-  useEffect(() => {
-
-  }, [slider])
 
   return (
     <div className="App">
       <header className="header"><h1>Slider React App</h1></header>
       <div className="container">
-        <div className="display">{slider}</div>
+        <div className="display" style={{ opacity: opacity }} ref={displayDiv}><h2 className="display_h2">{slider}%</h2></div>
         <label htmlFor='slider'></label>
-        <input type="range" className="slider" min="0" max="50" defaultValue={slider} id="slider" name="slider" onMouseUp={blurHandler} onInput={slideHandler} ref={sliderThumb} />
+        <input type="range" className="slider" min="0" max="100" defaultValue={slider} id="slider" name="slider" onMouseUp={mouseUpHandler} onInput={slideHandler} ref={sliderThumb} />
       </div>
     </div>
   );
